cf7754aad1efd9c39ac4457d3ad9ae3e72288873
Mykhail Myshyn <m.misha0307@gmail.com>
2025-12-03 00:00:21 +0200

    refactor: styles & theme-provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { withTheme } from './app/providers/ThemeProvider/theme-provider';
+import { useTheme, withTheme } from './app/providers/ThemeProvider/theme-provider';
 
 function App() {
-  return <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>;
+  const { theme, setTheme } = useTheme();
+  return (
+    <div>
+      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+
+      <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>Click</button>
+    </div>
+  );
 }
 
 export default withTheme(App);
