a648bb1f154b63d84a2497dadfb0b912c5181d38
Mykhail Myshyn <m.misha0307@gmail.com>
2025-12-02 18:19:57 +0200

    fix: comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import { withTheme } from './app/providers/ThemeProvider/theme-provider';
 
 function App() {
-  return (
-    <p className='read-the-docs'>
-      Click on the Vite and React logos to learn more
-    </p>
-  );
+  return <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>;
 }
 
 export default withTheme(App);
