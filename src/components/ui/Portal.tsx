1591b8dd57f86fb4555aca1460dbf9ec0067c689
Mykhail Myshyn <m.misha0307@gmail.com>
2025-12-04 15:00:17 +0200

    refactor: remove components

--- src/components/ui/Portal.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC, PropsWithChildren } from 'react';
-import { createPortal } from 'react-dom';
-
-type PortalProps = PropsWithChildren & {
-  element?: HTMLElement;
-};
-
-export const Portal: FC<PortalProps> = ({ children, element = document.body }) => createPortal(children, element);
