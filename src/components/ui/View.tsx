19a6da198ade027ffd1a0de63e767f0f930bb33e
Mykhail Myshyn <m.misha0307@gmail.com>
2025-12-04 14:27:55 +0200

    refactor: remove hooks

--- src/components/ui/View.tsx
+++ src/components/ui/View.tsx
@@ -12,26 +12,6 @@ const Condition = ({ if: condition, children }: ConditionProps) => {
   return null;
 };
 
-const Desktop = ({ children }: PropsWithChildren) => {
-  return <div className='hidden lg:block'>{children}</div>;
-};
-
-const Tablet = ({ children }: PropsWithChildren) => {
-  return <div className='hidden md:block'>{children}</div>;
-};
-
-const Mobile = ({ children }: PropsWithChildren) => {
-  return <div className='block md:hidden'>{children}</div>;
-};
-
-const TabletMobile = ({ children }: PropsWithChildren) => {
-  return <div className='block lg:hidden'>{children}</div>;
-};
-
 export const View = {
-  Condition,
-  Desktop,
-  Tablet,
-  Mobile,
-  TabletMobile
+  Condition
 };
