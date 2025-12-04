dc7580d9df10f2d5d6c3f6c1012b896facb4f5b5
Mykhail Myshyn <m.misha0307@gmail.com>
2025-12-04 18:02:40 +0200

    feat: update modal

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,2 +1,3 @@
+export { default as CloseIcon } from './close.svg';
 export { default as ExternalLinkIcon } from './external-link.svg';
 export { default as InfoIcon } from './info.svg';
--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,14 +1,21 @@
 import { type PropsWithChildren } from 'react';
 import { useEffect } from 'react';
 
+import { CloseIcon } from '@/assets/svg';
+import { HStack } from '@/components/common/HStack';
+import { Text } from '@/components/ui/Text';
+
 import { Portal } from '../common/Portal';
 
 export type ModalProps = PropsWithChildren & {
   open: boolean;
+
+  title: string;
+
   onClose: () => void;
 };
 
-export function Modal({ open, children, onClose }: ModalProps) {
+export function Modal({ open, title, children, onClose }: ModalProps) {
   useEffect(() => {
     function onKeyPress(e: any) {
       if (e.key === 'Escape') onClose();
@@ -23,5 +30,25 @@ export function Modal({ open, children, onClose }: ModalProps) {
 
   if (!open) return null;
 
-  return <Portal>{children}</Portal>;
+  return (
+    <Portal>
+      <HStack>
+        <Text
+          size='17'
+          weight='500'
+          lineHeight='20'
+          className='text-color-2'
+        >
+          {title}
+        </Text>
+        <CloseIcon
+          className='text-color-18'
+          onClick={onClose}
+          tabIndex={0}
+          aria-label='Close modal'
+        />
+      </HStack>
+      {children}
+    </Portal>
+  );
 }
