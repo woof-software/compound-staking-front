e1e89e4edd8e38eb5cb90b4f8819eb63700dd261
Mykhail Myshyn <m.misha0307@gmail.com>
2025-12-10 13:36:07 +0200

    fix: conflict

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 
-import { StakePage } from '@/pages/stake/StakePage';
+import { StakePage } from '@/pages/StakePage';
 
 export const Route = createFileRoute('/')({
   component: StakePage
