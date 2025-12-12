8bf0ee707bbfe5c32e8855b51f4cbf229bf71e8f
Mykhail Myshyn <misha.m@woof.software>
2025-12-12 04:28:35 +0200

    refactor: remove prop

--- src/components/ui/Skeleton.tsx
+++ src/components/ui/Skeleton.tsx
@@ -15,7 +15,6 @@ export function Skeleton(props: SkeletonProps) {
   if (loading) {
     return (
       <div
-        data-slot='skeleton'
         className={cn(skeletonClasses, className)}
         {...rest}
       >
@@ -26,7 +25,6 @@ export function Skeleton(props: SkeletonProps) {
 
   return (
     <div
-      data-slot='skeleton'
       className={cn(className)}
       {...rest}
     >
