aa2a8f5a4e9ba942694d4caee7116f9fd5830663
Mykhailo Myshyn <65620172+mmo0307@users.noreply.github.com>
2025-12-12 14:02:43 +0200

    Delegate (#16)
    * feat: add the first block & additions utils and hooks
    
    * feat: add StatisticsBlock & StakeFlowBlock
    
    * feat: add UnStakeFlowBlock & Tooltip
    
    * feat: add DelegateFlowBlock
    
    * feat: add useErc20Balance hook
    
    * feat: update modal
    
    * fix: conflicts
    
    * fix: conflicts
    
    * fix: input ref
    
    * fix: code
    
    * feat: add Modal styles
    
    * feat: connect wallet component & logic
    
    * feat: add logic for disconnect and change wallet
    
    * feat: add logic for pending
    
    * feat: format balance
    
    * feat: stake logic & additional stake
    
    * feat: add addresses and abi
    
    * refactor: useStateDev hook
    
    * refactor: useStateDev hook
    
    * refactor: remove comments
    
    * feat: switch network modal
    
    * feat: switch network modal
    
    * feat: rewards block
    
    * feat: vesting modal
    
    * feat: vesting modal
    
    * feat: claim modal
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: comments
    
    * fix: conflicts
    
    * feat: no wallet connect
    
    * feat: skeleton
    
    * feat: delegate addresses
    
    * refactor: reward block style update
    
    * refactor: update styles
    
    ---------
    
    Co-authored-by: Mykhail Myshyn <misha.m@woof.software>

--- src/consts/common.ts
+++ src/consts/common.ts
@@ -13,3 +13,18 @@ export const BASE_TOKEN_ADDRESS = '0xe357a464D9e37a50A1BdD71560B5c69a333d301E';
 export const STAKED_TOKEN_DECIMALS = 18;
 export const STAKED_TOKEN_ADDRESS = '0x4fBD1F4A09Ca535533549293AEEA50fe8Cd23026';
 export const STAKING_VAULT_ADDRESS = '0x993f58396E489352f7B0Fc9aa824E4d53cbD44C1';
+
+export const DELEGATES = [
+  { name: 'Compound Foundation', address: '0xb06df4dd01a5c5782f360ada9345c87e86adae3d' },
+  { name: 'WOOF!', address: '0xd2A79F263eC55DBC7B724eCc20FC7448D4795a0C' },
+  { name: 'PGov', address: '0x3FB19771947072629C8EEE7995a2eF23B72d4C8A' },
+  { name: 'Arana', address: '0x0579A616689f7ed748dC07692A3F150D44b0CA09' },
+  { name: 'Gauntlet', address: '0x683a4F9915D6216f73d6Df50151725036bD26C02' },
+  { name: 'Avantgarde', address: '0xB49f8b8613bE240213C1827e2E576044fFEC7948' },
+  { name: 'Michigan Blockchain', address: '0x13BDaE8c5F0fC40231F0E6A4ad70196F59138548' },
+  { name: 'FranklinDAO', address: '0x070341aA5Ed571f0FB2c4a5641409B1A46b4961b' },
+  { name: 'allthecolors', address: '0x66cD62c6F8A4BB0Cd8720488BCBd1A6221B765F9' },
+  { name: 'Arr00', address: '0x2B384212EDc04Ae8bB41738D05BA20E33277bf33' },
+  { name: 'Wintermute', address: '0xB933AEe47C438f22DE0747D57fc239FE37878Dd1' },
+  { name: 'a16z', address: '0x9aa835bc7b8ce13b9b0c9764a52fbf71ac62ccf1' }
+];
