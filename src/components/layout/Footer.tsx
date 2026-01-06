7af2497b76c8484e3eb2f8f104c17a5a71d7b1bf
Mykhail Myshyn <misha.m@woof.software>
2026-01-06 17:17:55 +0200

    fix: footer styles

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,17 +4,17 @@ import { TALLY_GOV_URL, TERMS_URL } from '@/consts/common';
 export function Footer() {
   return (
     <footer className='mt-auto bg-transparent'>
-      <div className='border-border-1 flex items-center justify-between border-t py-5'>
-        <div className='flex basis-1/2 items-center gap-x-5.5'>
+      <div className='border-border-1 flex items-center justify-between border-t-[0.5px] py-5'>
+        <div className='flex h-4 basis-1/2 items-center gap-x-[1.33rem]'>
           <a
-            className='text-color-24 hover:text-color-7 text-sm leading-3.5 font-medium'
+            className='text-color-24 hover:text-color-7 text-sm leading-3.5 font-normal'
             href={TALLY_GOV_URL}
             target='_blank'
           >
             Governance
           </a>
           <a
-            className='text-color-24 hover:text-color-7 text-sm leading-3.5 font-medium'
+            className='text-color-24 hover:text-color-7 text-sm leading-3.5 font-normal'
             href={TERMS_URL}
             target='_blank'
           >
