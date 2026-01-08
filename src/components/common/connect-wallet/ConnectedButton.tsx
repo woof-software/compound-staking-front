181077efc7ea150ec91b0c7de6e8ec3836c2b2ab
Mykhail Myshyn <misha.m@woof.software>
2026-01-08 03:34:28 +0200

    fix: connect wallet

--- src/components/common/connect-wallet/ConnectedButton.tsx
+++ src/components/common/connect-wallet/ConnectedButton.tsx
@@ -50,21 +50,21 @@ export function ConnectedButton({ onChangeWallet: onWalletChange }: ConnectedBut
       <div
         ref={toggleRef}
         onClick={onOpen}
-        className='border-color-8 rounded-64 bg-color-11 flex h-11 max-w-fit cursor-pointer justify-end border-[0.25px]'
+        className='border-color-8 rounded-64 bg-color-11 shadow-20 flex h-11 max-w-fit cursor-pointer justify-end border-[0.25px]'
       >
-        <div className='bg-color-11 rounded-64 relative flex w-23 items-center justify-center gap-2 border-r-0 py-2 pl-3'>
+        <div className='rounded-64 relative flex w-23 items-center border-r-0 py-2 pl-4'>
           <CompoundWalletIcon className='size-6 flex-shrink-0' />
           <Text
             size='11'
             weight='500'
             lineHeight='16'
-            className='text-color-2'
+            className='text-color-2 ml-2'
           >
             0.0039
           </Text>
         </div>
         <Condition if={!isPending}>
-          <div className='border-color-8 bg-color-4 rounded-64 relative -top-[0.5px] -right-0.25 flex h-11 w-[118.852px] items-center justify-center gap-2 border-[0.25px] px-3 py-2 hover:brightness-90'>
+          <div className='border-color-8 shadow-25 bg-color-4 rounded-64 relative -top-[0.5px] -right-[0.25px] h-11 border-[0.25px] px-5 py-[13.5px] hover:brightness-90'>
             <Text
               size='11'
               weight='500'
@@ -76,7 +76,7 @@ export function ConnectedButton({ onChangeWallet: onWalletChange }: ConnectedBut
           </div>
         </Condition>
         <Condition if={isPending}>
-          <div className='bg-color-7 rounded-64 flex h-11 min-w-25 items-center justify-center gap-2'>
+          <div className='bg-color-7 rounded-64 relative -top-[0.5px] flex h-11 min-w-28 items-center justify-center gap-2 hover:brightness-90'>
             <Spinner className='size-4 flex-shrink-0 animate-spin' />
             <Text
               size='11'
