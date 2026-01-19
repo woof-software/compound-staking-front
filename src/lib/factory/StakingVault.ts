64dd07b69fcf700bf8264542dfed9d0b9f7e4416
Mykhailo Myshyn <misha.m@woof.software>
2026-01-19 18:23:48 +0200

    feat: type chain (#30)
    * fix: logic and ui bugs
    
    * feat: configure factory
    
    * feat: configure factory
    
    * refactor: moved provider and singer to wagmi a util file and created hook useStakingVaultContract
    
    * feat: update workflow

---
+++ src/lib/factory/StakingVault.ts
@@ -0,0 +1,17 @@
+import { StakingVault__factory } from '@woof-software/compound-staked-comp-artifacts/types/ethers-contracts/factories/StakingVault__factory.js';
+
+import { ENV } from '@/consts/env';
+import { getEthersProvider, getEthersSigner } from '@/lib/utils/wagmi';
+import { config } from '@/shared/config/wagmiConfig';
+
+export function getStakingVaultRead(chainId: number) {
+  const provider = getEthersProvider(config, { chainId });
+
+  return StakingVault__factory.connect(ENV.STAKING_VAULT_ADDRESS, provider);
+}
+
+export async function getStakingVaultWrite(chainId: number) {
+  const signer = await getEthersSigner(config, { chainId });
+
+  return StakingVault__factory.connect(ENV.STAKING_VAULT_ADDRESS, signer);
+}
