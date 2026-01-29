33ddac16fba8f93b2dbf143c6cae35138820400c
Mykhail Myshyn <misha.m@woof.software>
2026-01-29 11:03:22 +0200

    feat: switch network

--- src/app/providers/query-client-provider.tsx
+++ src/app/providers/query-client-provider.tsx
@@ -1,5 +1,9 @@
 import type { PropsWithChildren } from 'react';
+import { useConnection } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+import { SwitchNetworkModal } from '@/components/common/switch-network-modal/SwitchNetworkModal';
+import { APPLICATION_CHAIN } from '@/consts/common';
 
 export type QueryClientRootProviderProps = PropsWithChildren;
 
@@ -13,6 +17,21 @@ const queryClient = new QueryClient({
   }
 });
 
+function NetworkGuard({ children }: PropsWithChildren) {
+  const { isConnected, chainId } = useConnection();
+
+  if (!isConnected) return <>{children}</>;
+
+  if (chainId === APPLICATION_CHAIN) return <>{children}</>;
+
+  return null;
+}
+
 export function QueryClientRootProvider({ children }: QueryClientRootProviderProps) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NetworkGuard>{children}</NetworkGuard>
+      <SwitchNetworkModal />
+    </QueryClientProvider>
+  );
 }
