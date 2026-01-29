1d48b3f7f62017384a1e76a3ba7d528519283fb4
Mykhail Myshyn <misha.m@woof.software>
2026-01-29 11:16:21 +0200

    feat: switch network

--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -3,17 +3,21 @@ import type { Address } from 'viem';
 
 import { ENV } from '@/consts/env';
 
-export function getImgPath(src: string) {
-  return `/src/assets${src}`;
+export function getChainLogo(chainId?: number) {
+  if (!chainId) return '/assets/chains/fallback-chain.svg';
+
+  const isChainInclude = [1, 10, 130, 137, 2020, 5000, 8453, 42161, 43114, 59144, 534352].includes(chainId);
+
+  if (isChainInclude) {
+    return `/assets/chains/${chainId}.svg`;
+  } else {
+    return '/assets/chains/fallback-chain.svg';
+  }
 }
 
-export function getChainLogo(chainId: number) {
-  //TODO: add fallback
-  if (![1, 10, 130, 137, 2020, 5000, 8453, 42161, 43114, 59144, 534352].includes(chainId)) return;
-  return `/assets/chains/${chainId}.svg`;
-}
+export function getChainName(chainId?: number) {
+  if (!chainId) return 'Unknown';
 
-export function getChainName(chainId: number) {
   switch (chainId) {
     case 1:
       return 'Ethereum';
