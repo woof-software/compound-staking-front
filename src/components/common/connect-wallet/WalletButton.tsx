67489bd1714cbfd00be4dd6bfacb37d07174db90
nikita-woof <nikita.s@woof.software>
2026-02-04 18:01:49 +0200

    feat: tenderly integration (#44)
    * add tenderly integration
    
    * reload page on connect wallet click
    
    * remove connect wallet modal

--- src/components/common/connect-wallet/WalletButton.tsx
+++ src/components/common/connect-wallet/WalletButton.tsx
@@ -16,7 +16,7 @@ export function WalletButton() {
       <Condition if={!address}>
         <Button
           className='bg-color-4 rounded-64 text-color-2 border-color-7 w-auto max-w-30.5 border-[0.25px] px-5 py-[13.5px] text-[11px] font-medium'
-          onClick={onOpen}
+          onClick={() => window.location.reload()}
         >
           Connect Wallet
         </Button>
