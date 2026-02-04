dfe1a3373c8d81d0df54be4e271bdf3583047f24
Nikita <nikita.s@woof.software>
2026-02-04 14:15:16 +0200

    add tenderly integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
+import { useEffect, useRef } from 'react';
 import { createRoot } from 'react-dom/client';
-import { WagmiProvider } from 'wagmi';
+import { useAccount, useConnect, WagmiProvider } from 'wagmi';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 
@@ -17,9 +18,27 @@ declare module '@tanstack/react-router' {
   }
 }
 
+function AutoConnect() {
+  const { connectors, connect } = useConnect();
+  const { isConnected } = useAccount();
+  const triedToConnect = useRef(false);
+
+  useEffect(() => {
+    if (triedToConnect.current) return;
+
+    if (!isConnected && connectors.length > 0) {
+      connect({ connector: connectors[0]! });
+      triedToConnect.current = true;
+    }
+  }, [isConnected, connectors, connect]);
+
+  return null;
+}
+
 createRoot(document.getElementById('root')!).render(
   <WagmiProvider config={config}>
     <QueryClientRootProvider>
+      <AutoConnect />
       <RouterProvider router={router} />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientRootProvider>
--- src/shared/config/wagmiConfig.ts
+++ src/shared/config/wagmiConfig.ts
@@ -1,21 +1,129 @@
-import { createConfig, http } from 'wagmi';
-import { sepolia } from 'wagmi/chains';
-import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors';
+import { createWalletClient, type Hex, isHex } from 'viem';
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts';
+import { sepolia } from 'viem/chains';
+import { createConnector, http } from 'wagmi';
+import { createConfig } from 'wagmi';
+import { getDeployment } from '@woof-software/compound-staked-comp-artifacts/deployments';
 
-import { ENV } from '@/consts/env';
+// @ts-expect-error - we do not really care about it while having demo
+sepolia.id = 111555111;
 
-const ANKR_RPC = `https://rpc.ankr.com/eth_sepolia/${ENV.ANKR_RPC_KEY}`;
+// @ts-expect-error - we do not really care about it while having demo
+sepolia.name = 'Sandbox';
 
-export const config = createConfig({
-  chains: [sepolia],
-  connectors: [
-    injected(),
-    walletConnect({ projectId: ENV.WALLET_CONNECT_PROJECT_ID }),
-    coinbaseWallet({
-      appName: 'Compound III'
-    })
-  ],
-  transports: {
-    [sepolia.id]: http(ANKR_RPC)
+const STORAGE_KEY = 'pk';
+
+function createSandboxConnector() {
+  if (typeof window === 'undefined') {
+    return createConfig({
+      chains: [sepolia],
+      ssr: true,
+      transports: {
+        [sepolia.id]: http('https://virtual.sepolia.eu.rpc.tenderly.co/be13bd77-e574-49da-86c9-e3d2422cea28')
+      }
+    });
   }
-});
+
+  let privatekey = localStorage.getItem(STORAGE_KEY);
+
+  if (!privatekey) {
+    privatekey = generatePrivateKey();
+    localStorage.setItem(STORAGE_KEY, privatekey);
+    const { address } = privateKeyToAccount(<Hex>privatekey);
+
+    fetch('https://virtual.sepolia.eu.rpc.tenderly.co/5c22cdc8-568e-486c-88b5-76c7da6c29fb', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        jsonrpc: '2.0',
+        method: 'tenderly_setBalance',
+        params: [address, '0xDE0B6B3A7640000'],
+        id: 0
+      })
+    });
+
+    fetch('https://virtual.sepolia.eu.rpc.tenderly.co/5c22cdc8-568e-486c-88b5-76c7da6c29fb', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        jsonrpc: '2.0',
+        method: 'tenderly_setErc20Balance',
+        params: [getDeployment(11155111, 'MockComp').address, address, '0x8AC7230489E80000'],
+        id: 1
+      })
+    });
+  }
+
+  if (!isHex(privatekey)) {
+    throw new Error('Wrong private key format');
+  }
+
+  const forkUrl = 'https://virtual.sepolia.eu.rpc.tenderly.co/be13bd77-e574-49da-86c9-e3d2422cea28';
+  const forkChainId = 111555111;
+  const name = 'Sandbox';
+
+  const account = privateKeyToAccount(privatekey);
+
+  const walletClient = createWalletClient({
+    transport: http(forkUrl),
+    chain: {
+      ...sepolia,
+      id: forkChainId,
+      name,
+      rpcUrls: {
+        default: {
+          http: [forkUrl]
+        }
+      }
+    },
+    account
+  });
+
+  const sandboxConnector = createConnector(() => ({
+    id: 'sandbox',
+    name: 'Sandbox Wallet',
+    type: 'sandbox',
+    async connect(parameters) {
+      const withCapabilities = parameters?.withCapabilities ?? false;
+
+      return {
+        accounts: (withCapabilities ? [{ address: account.address, capabilities: {} }] : [account.address]) as never,
+        chainId: forkChainId
+      };
+    },
+    async getAccounts() {
+      return [account.address];
+    },
+    async getChainId() {
+      return forkChainId;
+    },
+    async isAuthorized() {
+      return true;
+    },
+    async getClient() {
+      return walletClient;
+    },
+    async getProvider() {
+      return walletClient.transport;
+    },
+    async disconnect() {},
+    onAccountsChanged() {},
+    onChainChanged() {},
+    onDisconnect() {}
+  }));
+
+  return createConfig({
+    connectors: [sandboxConnector],
+    chains: [sepolia],
+    ssr: true,
+    transports: {
+      [sepolia.id]: http(forkUrl)
+    }
+  });
+}
+
+export const config = createSandboxConnector();
