17f45d75b6b839588436493b8c2414368b6ad3d9
Stanislav Baraniuk <stas.b@woof.software>
2026-02-23 19:00:01 +0100

    feat: implement APR fetching

--- src/pages/stake/hooks/useStatisticStakingAPR.ts
+++ src/pages/stake/hooks/useStatisticStakingAPR.ts
@@ -1,7 +1,25 @@
-export function useStatisticStakingAPR() {
-  const stakingAPR = 6.53;
+import { z } from 'zod';
+import { useQuery } from '@tanstack/react-query';
+
+import { useStakingVaultContract } from '@/hooks/useStakingVaultContract';
+import { queryKeys } from '@/shared/query-keys';
+
+export function useStatisticStakingAPR(chainId?: number) {
+  const { read } = useStakingVaultContract(chainId);
+
+  const { data, ...query } = useQuery<bigint | undefined>({
+    queryKey: queryKeys.stakingVault.baseApr([chainId]),
+    queryFn: () => {
+      if (!read) return;
+
+      return read.getBaseAprWad();
+    }
+  });
+
+  const schema = z.bigint().optional();
 
   return {
-    stakingAPR
+    data: schema.parse(data),
+    ...query
   };
 }
--- src/shared/query-keys.ts
+++ src/shared/query-keys.ts
@@ -5,7 +5,8 @@ export const queryKeys = {
     virtualBalanceOf: (...args: unknown[]) => ['stakingVault', 'virtualBalanceOf', ...args],
     multiplierOf: (...args: unknown[]) => ['stakingVault', 'multiplierOf', ...args],
     availableRewardsOf: (...args: unknown[]) => ['stakingVault', 'availableRewardsOf', ...args],
-    totalStaked: (...args: unknown[]) => ['stakingVault', 'totalStaked', ...args]
+    totalStaked: (...args: unknown[]) => ['stakingVault', 'totalStaked', ...args],
+    baseApr: (...args: unknown[]) => ['stakingVault', 'baseApr', ...args]
   },
   lockManager: {
     root: () => ['lockManager'],
